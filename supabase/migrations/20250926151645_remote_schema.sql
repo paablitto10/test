a61230a4a8507d9f42361dd52334edde2e048617
jonypopovv <jonypopovv@gmail.com>
2025-09-26 21:36:01 +0500

    fix: update supabase/migrations with current schema

--- supabase/migrations/20250926151645_remote_schema.sql
+++ supabase/migrations/20250926151645_remote_schema.sql
@@ -426,3 +426,69 @@ ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TAB
 
 
 RESET ALL;
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
+
+
+  create policy "Anyone can delete their avatar. 1oj01fe_0"
+  on "storage"."objects"
+  as permissive
+  for delete
+  to public
+using ((bucket_id = 'avatars'::text));
+
+
+
+  create policy "Anyone can upload an avatar. 1oj01fe_0"
+  on "storage"."objects"
+  as permissive
+  for insert
+  to public
+with check ((bucket_id = 'avatars'::text));
+
+
+
+  create policy "Anyone can upload an avatar."
+  on "storage"."objects"
+  as permissive
+  for insert
+  to public
+with check ((bucket_id = 'avatars'::text));
+
+
+
+  create policy "Avatar images are publicly accessible."
+  on "storage"."objects"
+  as permissive
+  for select
+  to public
+using ((bucket_id = 'avatars'::text));
+
+
+
+  create policy "Give users access to own folder 1gncj5w_0"
+  on "storage"."objects"
+  as permissive
+  for select
+  to public
+using (((bucket_id = 'notes-images'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
+
+
+
+  create policy "Give users access to own folder 1gncj5w_1"
+  on "storage"."objects"
+  as permissive
+  for insert
+  to public
+with check (((bucket_id = 'notes-images'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
+
+
+
+  create policy "Give users access to own folder delete 1gncj5w_0"
+  on "storage"."objects"
+  as permissive
+  for delete
+  to public
+using (((bucket_id = 'notes-images'::text) AND (( SELECT (auth.uid())::text AS uid) = (storage.foldername(name))[1])));
+
+
+
